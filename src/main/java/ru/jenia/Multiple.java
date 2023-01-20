f66595a62dbe76c5088027e3aff8380dcb96c35c
evgmokrushina <evgmokrushina>
2023-01-20 18:27:36 +0500

    Parsing the program

---
+++ src/main/java/ru/jenia/Multiple.java
@@ -0,0 +1,16 @@
+package ru.jenia;
+
+public class Multiple {
+
+    public static void main(String[] args) {
+        System.out.println("5 * 1 = 5");
+        System.out.println("5 * 2 = 10");
+        System.out.println("5 * 3 = 15");
+        System.out.println("5 * 4 = 20");
+        System.out.println("5 * 5 = 25");
+        System.out.println("5 * 6 = 30");
+        System.out.println("5 * 7 = 35");
+        System.out.println("5 * 8 = 40");
+        System.out.println("5 * 9 = 45");
+    }
+}
