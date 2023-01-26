5321722f24e70aec35605fb713cb2fea655065e3
evgmokrushina <evgmokrushina>
2023-01-26 17:55:57 +0500

    Currency converter

---
+++ src/main/java/ru/jenia/converter/Converter.java
@@ -0,0 +1,24 @@
+package ru.jenia.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        float rs1 = value / 70;
+        return rs1;
+
+    }
+
+    public static float rubleToDollar(float value) {
+        float rs1 = value / 60;
+        return rs1;
+
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro");
+
+        float dollar = Converter.rubleToDollar(140);
+        System.out.println("140 rubles are " + dollar + " dollar");
+    }
+}
