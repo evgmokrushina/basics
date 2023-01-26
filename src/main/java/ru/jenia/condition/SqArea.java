21981c1382018af2fd1a060708986694bb1766c3
evgmokrushina <evgmokrushina>
2023-01-26 18:49:33 +0500

    Sides of a rectangle

---
+++ src/main/java/ru/jenia/condition/SqArea.java
@@ -0,0 +1,16 @@
+package ru.jenia.condition;
+
+public class SqArea {
+    public static double square(double p, double k) {
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double rsl = l * h;
+        return rsl;
+
+    }
+
+    public static void main(String[] args) {
+        double result = SqArea.square(6, 2);
+        System.out.println("p = 6, k = 2, s = 2, real = " + result);
+    }
+}
