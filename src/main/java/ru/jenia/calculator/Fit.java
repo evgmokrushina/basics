9cbcb7caad5cbaf9f036d871f660ff96910d9f7a
evgmokrushina <evgmokrushina>
2023-01-26 18:18:04 +0500

    Ideal weight

---
+++ src/main/java/ru/jenia/calculator/Fit.java
@@ -0,0 +1,25 @@
+package ru.jenia.calculator;
+
+public class Fit {
+    public static double manWeight(short height) {
+        double rsl = (height - 100) * 1.15;
+        return rsl;
+
+    }
+
+    public static double womanWeight(short height) {
+        double rsl = (height - 110) * 1.15;
+        return rsl;
+
+    }
+
+    public static void main(String[] args) {
+        short height = 190;
+        double man = Fit.manWeight(height);
+        System.out.println("Man 190 is " + man);
+
+        short height1 = 156;
+        double woman = Fit.womanWeight(height1);
+        System.out.println("Woman 156 is " + woman);
+    }
+}
