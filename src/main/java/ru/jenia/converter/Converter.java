2628e7f39c02d2b0db73c9d93aa8e5a65fdfd5a2
evgmokrushina <evgmokrushina>
2023-01-30 21:20:35 +0500

    What is testing fixed

--- src/main/java/ru/jenia/converter/Converter.java
+++ src/main/java/ru/jenia/converter/Converter.java
@@ -3,33 +3,30 @@ package ru.jenia.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        float rs1 = value / 70;
-        return rs1;
-
+        float rsl = value / 70;
+        return rsl;
     }
 
     public static float rubleToDollar(float value) {
-        float rs1 = value / 60;
-        return rs1;
-
+        float rsl = value / 60;
+        return rsl;
     }
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro");
-
         float in = 140;
         float expected = 2;
         float out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2 euro. Test result: " + passed);
-
-        float dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " dollar");
-
-        float out1 = Converter.rubleToDollar(in);
-        boolean passed1 = expected == out1;
-        System.out.println("140 rubles are 2 dollar. Test result: " + passed1);
+        float dollar = Converter.rubleToDollar(180);
+        System.out.println("180 rubles are " + dollar + " dollar");
+        in = 180;
+        expected = 3;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("180 rubles are 3 dollar. Test result: " + passed);
     }
 
 }
