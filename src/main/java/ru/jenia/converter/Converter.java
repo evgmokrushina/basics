dd93d1705b1f17d48beffb809395248fde23d123
evgmokrushina <evgmokrushina>
2023-01-30 20:31:16 +0500

    What is testing

--- src/main/java/ru/jenia/converter/Converter.java
+++ src/main/java/ru/jenia/converter/Converter.java
@@ -18,7 +18,18 @@ public class Converter {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro");
 
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2 euro. Test result: " + passed);
+
         float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + dollar + " dollar");
+
+        float out1 = Converter.rubleToDollar(in);
+        boolean passed1 = expected == out1;
+        System.out.println("140 rubles are 2 dollar. Test result: " + passed1);
     }
+
 }
