3ad2604fc765573130ca9dd276bf3a6b8c545865
evgmokrushina <evgmokrushina>
2023-02-11 19:46:38 +0500

    Stupid bot corrected

--- src/test/java/ru/jenia/condition/DummyBot.java
+++ src/test/java/ru/jenia/condition/DummyBot.java
@@ -2,14 +2,14 @@ package ru.jenia.condition;
 
 public class DummyBot {
     public static String answer(String question) {
-        String rsl = "I don't know. Please, ask another question.";
+        String rsl;
         if ("Hi, Bot.".equals(question)) {
             rsl = "Hi, SmartAss.";
-        } else {
-            if ("Bye.".equals(question)) {
+        } else if ("Bye.".equals(question)) {
                 rsl = "See you later.";
+            } else {
+                rsl = "I don't know. Please, ask another question.";
             }
-        }
         return rsl;
     }
 
