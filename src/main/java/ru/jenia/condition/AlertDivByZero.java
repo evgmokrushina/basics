dd020d1e48525e2efc4712075aded771626c0e84
evgmokrushina <evgmokrushina>
2023-02-11 21:16:34 +0500

    Stupid bot fixed1

--- src/main/java/ru/jenia/condition/AlertDivByZero.java
+++ src/main/java/ru/jenia/condition/AlertDivByZero.java
@@ -4,7 +4,7 @@ public class AlertDivByZero {
     public static void possibleDiv(int number) {
         if (number == 0) {
             System.out.println("Could not div by 0.");
-            }
+        }
         if (number < 0) {
             System.out.println("This is negative number");
         }
