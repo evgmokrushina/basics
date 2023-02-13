8a65b55871608b2a8becf5ba5c7762d618c0a10a
evgmokrushina <evgmokrushina>
2023-02-13 18:50:45 +0500

    Multiple boolean expression AND

---
+++ src/main/java/ru/jenia/condition/Triangle.java
@@ -0,0 +1,12 @@
+package ru.jenia.condition;
+
+public class Triangle {
+    public static boolean exist(double ab, double ac, double bc) {
+        return (ab + ac > bc) && (ac + bc > ab) && (ab + bc > ac);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(exist(2.0, 2.0, 2.0));
+        System.out.println(exist(3.0, 2.0, 1.0));
+    }
+}
