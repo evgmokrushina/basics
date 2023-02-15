608e25d53a0ffa9c45077a5ebfdcf8e4f1001f91
evgmokrushina <evgmokrushina>
2023-02-15 19:17:30 +0500

    Math.abs corrected

--- src/main/java/ru/jenia/condition/ChessBoard.java
+++ src/main/java/ru/jenia/condition/ChessBoard.java
@@ -6,16 +6,14 @@ public class ChessBoard {
         int rsl = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if (x1 != x2 && y1 != y2) {
                 int deltaX = Math.abs(x2 - x1);
                 int deltaY = Math.abs(y2 - y1);
                 rsl =  deltaX == deltaY ? deltaX : rsl;
             }
-        }
         return rsl;
     }
 
     private static boolean isValid(int coordinate) {
-        return coordinate >= 0 && coordinate <= 7;
+    return coordinate >= 0 && coordinate <= 7;
     }
 }
