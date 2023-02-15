53a8df2906996847f48d6a946e37637fc999a433
evgmokrushina <evgmokrushina>
2023-02-15 20:29:09 +0500

    Nested if else statement. obfuscated code corrected

--- src/main/java/ru/jenia/condition/DivideBySix.java
+++ src/main/java/ru/jenia/condition/DivideBySix.java
@@ -4,9 +4,11 @@ public class DivideBySix {
     public static String checkNumber(int number) {
         String rsl;
         if (number % 3 == 0) {
-            rsl = number % 2 == 0 ? "The number divides by 6." : "The number divides by 3, but it isn't the even number.";
+            rsl = number % 2 == 0 ? "The number divides by 6."
+                    : "The number divides by 3, but it isn't the even number.";
         } else {
-            rsl = number % 2 == 0 ? "The number doesn't divide by 3, but it is the even number." : "The number doesn't divide by 3 and it isn't the even number.";
+            rsl = number % 2 == 0 ? "The number doesn't divide by 3, but it is the even number."
+                    : "The number doesn't divide by 3 and it isn't the even number.";
         }
         return rsl;
     }
