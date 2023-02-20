553828d3e9070c77fb2eea5d22a69156cfd2253a
evgmokrushina <evgmokrushina>
2023-02-20 18:16:43 +0500

    Write a program that calculates factorial

---
+++ src/main/java/ru/jenia/loop/Factorial.java
@@ -0,0 +1,14 @@
+package ru.jenia.loop;
+
+public class Factorial {
+    public static int calc(int n) {
+        int result = 0;
+        if (result <= 1) {
+            result = result + 1;
+            for (int i = 1; i <= n; i++) {
+                result = result * i;
+            }
+        }
+        return result;
+    }
+}
