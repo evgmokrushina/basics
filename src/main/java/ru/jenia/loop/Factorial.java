db27bebfea48261813fa00ba997070bfc029d9ce
evgmokrushina <evgmokrushina>
2023-02-20 18:23:29 +0500

    Write a program that calculates factorial corrected

--- src/main/java/ru/jenia/loop/Factorial.java
+++ src/main/java/ru/jenia/loop/Factorial.java
@@ -4,7 +4,7 @@ public class Factorial {
     public static int calc(int n) {
         int result = 0;
         if (result <= 1) {
-            result = result + 1;
+            result++;
             for (int i = 1; i <= n; i++) {
                 result = result * i;
             }
