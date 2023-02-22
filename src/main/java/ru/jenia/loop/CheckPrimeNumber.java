86c68800eaaca6f9b51507ebd6d31c2a5641cebb
evgmokrushina <evgmokrushina>
2023-02-22 20:59:35 +0500

    Prime number corrected

--- src/main/java/ru/jenia/loop/CheckPrimeNumber.java
+++ src/main/java/ru/jenia/loop/CheckPrimeNumber.java
@@ -2,11 +2,10 @@ package ru.jenia.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        if (number <= 1) {
-            return false;
+        boolean prime = false;
+        if (number > 1) {
+            prime = true;
         }
-
-        boolean prime = true;
         for (int i = 2; i <= (number - 1); i++) {
             if ((number % i) == 0) {
                 prime = false;
