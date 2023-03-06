195ef3ff29fa2a862a936847b2968667686e7b31
evgmokrushina <evgmokrushina>
2023-03-06 18:41:20 +0500

    Traversing an array from the last element corr

--- src/main/java/ru/jenia/array/RollBackArray.java
+++ src/main/java/ru/jenia/array/RollBackArray.java
@@ -4,9 +4,9 @@ public class RollBackArray {
     public static int[] rollback(int[] array) {
         int[] result = new int[array.length];
         for (int index = 0; index < array.length; index++) {
-            int lastIndex = array.length - 1;
-            int currentIndex = lastIndex - index;
-            result[currentIndex] = array[index];
+            int lastIndex = array.length - 1 - index;
+            int currentIndex = index;
+            result[currentIndex] = array[lastIndex];
         }
         return result;
     }
