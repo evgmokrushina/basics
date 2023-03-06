61a71f58092c03336770652057aaa60e4d429104
evgmokrushina <evgmokrushina>
2023-03-06 18:05:59 +0500

    The word starts with...corr

--- src/main/java/ru/jenia/array/ArrayChar.java
+++ src/main/java/ru/jenia/array/ArrayChar.java
@@ -3,14 +3,10 @@ package ru.jenia.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        for (int i = 0; i < word.length; i++) {
-            for (int j = 0; j < pref.length; j++) {
-                if (i == j) {
-                    if (word[i] != pref[j]) {
-                        result = false;
-                        break;
-                    }
-                }
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
+                result = false;
+                break;
             }
         }
         return result;
