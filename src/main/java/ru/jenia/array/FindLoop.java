7b3112ee842314cc1e91be2ef06e5a2307b08ccd
evgmokrushina <evgmokrushina>
2023-03-09 18:00:47 +0500

    Finding an index in a range corrected

--- src/main/java/ru/jenia/array/FindLoop.java
+++ src/main/java/ru/jenia/array/FindLoop.java
@@ -14,9 +14,9 @@ public class FindLoop {
 
     public static int indexInRange(int[] data, int el, int start, int finish) {
         int rst = -1;
-        for (int i = start; i <= finish; i++) {
-            if (data[i] == el) {
-                rst = i;
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == el) {
+                rst = index;
                 break;
             }
         }
